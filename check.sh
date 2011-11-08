b49dfbddf2673172ebf587f434c2ba114a5c28a5
Justin Riley <justin.t.riley@gmail.com>
2011-11-08 11:11:54 -0500

    replace clean.sh/check.sh with python versions

--- check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SRC=$(dirname $0)/starcluster_plugins
-echo ">>> Running pyflakes..."
-find $SRC -iname \*.py -exec pyflakes {} \;
-echo ">>> Running pep8..."
-find $SRC -iname \*.py -exec pep8 {} \;
-echo ">>> Done"
--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SRC=$(dirname $0)/starcluster_plugins
-find $SRC -iname \*.pyc -exec rm {} \;
-find $SRC -iname \*.pyo -exec rm {} \;
